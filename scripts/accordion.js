b15db9a4bc5e5dbe63f73970c103c4cc97d68e3f
Antonis <tonymis.16@hotmail.com>
2017-03-20 00:43:13 +0200

    Script accordion

---
+++ scripts/accordion.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+		$("#menu").accordion({collapsible: true, active: false,heightStyle: "content"});
+		$(".ui-accordion-content").css("background-color", "#CACACA");
+		$(".ui-widget-content").css("background", "#CACACA");
+});
